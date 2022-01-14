8535e850c712eacd3a7916ece7229e6874293291
Jimin Kim <0305jimin@gmail.com>
2022-01-15 02:50:17 +0900

    day 5: refactoring
    classList.contains(),remove,add

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,11 @@ const h1 = document.querySelector("div.hello:first-child h1"); //1. find the ele
 
 function handleTitleClick() {//3. react to the event
     const clickedClass="active";//use less raw values
-  if(h1.className===clickedClass){
-      h1.className = "";
+  if(h1.classList.contains(clickedClass)){
+      h1.classList.remove(clickedClass);
   }else{
-      h1.className=clickedClass;
-  }
+    h1.classList.add(clickedClass);  
+}
 }
 
 h1.addEventListener("click", handleTitleClick);// 2. make the element listen to the event
