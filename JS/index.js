4ad50450c3443722061c8f3c0935c400999f9cf2
Jimin Kim <0305jimin@gmail.com>
2022-01-15 02:45:09 +0900

    day 5: refactoring

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1"); //1. find the element
 
 function handleTitleClick() {//3. react to the event
-  const currentColor = h1.style.color;
-  let newColor;
-  
-  if (currentColor === "blue") {
-    newColor= "tomato";
-  } else {
-    newColor = "blue";
+    const clickedClass="active";//use less raw values
+  if(h1.className===clickedClass){
+      h1.className = "";
+  }else{
+      h1.className=clickedClass;
   }
-  h1.style.color=newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);// 2. make the element listen to the event
