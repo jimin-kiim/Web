fbe40c78aa9dbf341986997307b13c8d1fbc7487
Jimin Kim <0305jimin@gmail.com>
2022-01-15 02:38:11 +0900

    day 5: addEventListener

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,15 @@
-const age = parseInt( prompt("How old are you?"));//parseInt: string -> int if the string doesn't represent a numeric, it returns NaN 
-console.log(isNaN(age));//isNaN returns boolean.
+const h1 = document.querySelector("div.hello:first-child h1"); //1. find the element
 
-if (isNaN(age)){
-    console.log("Please write a number");
-}else{
-    console.log("Thank you for writing you age");
-
+function handleTitleClick() {//3. react to the event
+  const currentColor = h1.style.color;
+  let newColor;
+  
+  if (currentColor === "blue") {
+    newColor= "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color=newColor;
 }
+
+h1.addEventListener("click", handleTitleClick);// 2. make the element listen to the event
