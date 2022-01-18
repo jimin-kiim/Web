80b02abeec18f5ab66ee98d028922e5c9775a08b
Jimin Kim <0305jimin@gmail.com>
2022-01-19 01:36:45 +0900

    day 7-8: link preventDefault

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
   event.preventDefault();//stopping the default behavior of any event
+  // default behavior of form: submit
   console.log(event);
 }
+function handleLinkClick(event){
+  event.preventDefault();
+  // alert("clicked!");
+  console.log(event);
+
+}
+// default behavior of link: click and go somewhere
 loginForm.addEventListener("submit", onLoginSubmit)
-// function(): executing right away. 
-// function: not yet.
+link.addEventListener("click",handleLinkClick)
