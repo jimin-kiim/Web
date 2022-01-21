f807f5caf3051e3786a22784804a514f17e25183
Jimin Kim <0305jimin@gmail.com>
2022-01-21 20:30:06 +0900

    feat: saving the todo list
    JSON.parse, JSON.stringify, array.forEach, arrow function,

--- JS/nomadCoder/js/todo.js
+++ JS/nomadCoder/js/todo.js
@@ -2,10 +2,18 @@ const toDoForm= document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const TODOS_KEY ="todos";
+let toDos =[];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
 }
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -22,7 +30,34 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo= toDoInput.value;
     toDoInput.value = "";
+    toDos.push(toDoInput);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+// HOW TO WRITE A FUNCTION 1
+
+// function sayHello(item){
+//     console.log("this is the turn of ",item);
+// }
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+// if(saveToDos){
+//     const parsedToDos = JSON.parse(saveToDos);
+//     parsedToDos.forEach(sayHello);
+// }
+
+// HOW TO WRITE A FUNCTION 2
+
+// if(saveToDos){
+//     const parsedToDos = JSON.parse(saveToDos);
+//     parsedToDos.forEach((item)=> console.log("this is the turn of ",item));
+// }
+
+if(saveToDos){
+    const parsedToDos = JSON.parse(saveToDos);
+    toDos= parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
