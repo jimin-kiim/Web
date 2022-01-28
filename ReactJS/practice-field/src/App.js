377cc6398434c619b746370f613dd008511fa5bf
Jimin Kim <0305jimin@gmail.com>
2022-01-28 20:08:03 +0900

    feat: calculating how many coins I can afford
    replace(/[^0-9.]/g, "")

--- ReactJS/practice-field/src/App.js
+++ ReactJS/practice-field/src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
+    const [amount, setInput] = useState("");
+    const [select, setSelect] = useState(false);
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
@@ -11,18 +13,38 @@ function App() {
                 setLoading(false);
             });
     }, []);
+    const onChange = (event) => setInput(event.target.value);
+    const onSelect = (event) =>
+        setSelect(event.target.value.replace(/[^0-9.]/g, ""));
     return (
         <div>
-            <h1>The Coins!({coins.length})</h1>
-            {loading ? <strong>loading</strong> : null}
-            <ul>
-                {coins.map((coin) => (
-                    <li key={coin.id}>
-                        {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-                        USD
-                    </li>
-                ))}
-            </ul>
+            <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+            {loading ? (
+                <strong>loading</strong>
+            ) : (
+                <div>
+                    <div>
+                        <span>"I have </span>
+                        <input value={amount} onChange={onChange}></input>
+                        <span> dollars."</span>
+                    </div>
+                    <span>"I want to buy </span>
+                    <select onChange={onSelect}>
+                        {coins.map((coin) => (
+                            <option key={coin.id}>
+                                {coin.name} ({coin.symbol}) : $
+                                {coin.quotes.USD.price.toFixed(2)}
+                                USD
+                            </option>
+                        ))}
+                        ;
+                    </select>
+                    <span>"</span>
+                    {select ? (
+                        <h3>You can buy {Math.floor(amount / select)} coin</h3>
+                    ) : null}
+                </div>
+            )}
         </div>
     );
 }
