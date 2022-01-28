3c11db607312dc211f728ec927dd89e1b58472f0
Jimin Kim <0305jimin@gmail.com>
2022-01-28 18:19:58 +0900

    feat: toDoList

--- ReactJS/practice-field/src/App.js
+++ ReactJS/practice-field/src/App.js
@@ -1,29 +1,36 @@
 import { useState, useEffect } from "react";
-function Hello() {
-    // function byeFn() {
-    //     console.log("bye :(");
-    // }
-    // function hiFn() {
-    //     console.log("hi :)");
-    //     return byeFn; //clean up function
-    // }
-    useEffect(() => {
-        //when the <Hello /> comes up again, this function also runs again
-        console.log("created :)");
-        return () => console.log("destroyed :("); //when the component is destroyed
-    }, []);
-    // useEffect(hiFn, []); //allows us to choose when to run the code
-    return <h1>Helllo</h1>;
-}
+
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev); //recieve the previous value and make it opposite.
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]); //[toDo, toDos]: array inside array case.
+        setToDo("");
+    };
     return (
         <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+            <h1>My To Dos ([toDos.length])</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={toDo}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item) => (
+                    <li key={index}>{(item, index)}</li>
+                ))}
+            </ul>
         </div>
     );
 }
 export default App;
-// when we write in javascript, we should use curly braces {}
