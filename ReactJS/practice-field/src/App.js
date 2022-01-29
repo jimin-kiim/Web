28cac90619a2f79cd81324e5c077cc4cec37d1fe
Jimin Kim <0305jimin@gmail.com>
2022-01-29 23:28:00 +0900

    feat: link Detail and Home components
    react-dom-router, Router, Switch, Route, path

--- ReactJS/practice-field/src/App.js
+++ ReactJS/practice-field/src/App.js
@@ -1,46 +1,20 @@
-import { useState, useEffect } from "react";
-import Movie from "./Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-    useEffect(() => {
-        // fetch(
-        //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-        // )
-        //     .then((response) => response.json())
-        //     .then((json) => {
-        //         setMovies(json.data.movies);
-        //         setLoading(false);
-        //     });
-        getMovies();
-    }, []);
     return (
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map((movie) => (
-                        <Movie
-                            key={movie.id}
-                            coverImg={movie.medium_cover_image}
-                            title={movie.title}
-                            summary={movie.summary}
-                            genres={movie.genres}
-                        />
-                    ))}
-                </div>
-            )}
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/movie/:id">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
-}
+} //looking at the url
 export default App;
+// Browser Router: /movie
+//HashRouter: /#/movie
